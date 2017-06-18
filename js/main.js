f0fe7f7a76b73b54fd86e27d5024e72ba2612bfc
ishmilan <isma.milan.marquez@gmail.com>
2017-06-18 17:27:51 +0200

    .

--- js/main.js
+++ js/main.js
@@ -1,25 +1,17 @@
 window.addEventListener("load",function(){
-    /*SCROLL TOP*/
-    var btn = document.createElement("a");
-    btn.id="scroll-top";
-    btn.href="#start";
-    var t= document.createTextNode("➤");
-    btn.appendChild(t);
-    document.body.appendChild(btn);
-    document.addEventListener("scroll",function() {
-        document.getElementById("scroll-top").style.bottom = "50px";
-    });
-    document.getElementById("scroll-top").addEventListener("click",function myFunction() {
-        setTimeout(function() {document.getElementById("scroll-top").style.bottom = "-80px";},200);
-    });
-    /*SHOW CONTACT FOOTER*/
-    document.getElementById("contact").addEventListener("click",function(e){
-        e.preventDefault();
-        var f=document.getElementById("footer");
-        if (f.style.bottom!="0px"){
-            f.style.bottom="0px";
-        }else{
-            f.style.bottom="-80px"
-        }
-    });
+/*SCROLL TOP*/
+var btn = document.createElement("a");
+btn.id="scroll-top";
+btn.href="#start";
+var t= document.createTextNode("➤");
+btn.appendChild(t);
+document.body.appendChild(btn);
+document.addEventListener("scroll",function(){document.getElementById("scroll-top").style.bottom = "30px";});
+document.getElementById("scroll-top").addEventListener("click",function myFunction(){setTimeout(function(){document.getElementById("scroll-top").style.bottom="-80px";},200);});
+/*SHOW CONTACT FOOTER*/
+document.getElementById("contact").addEventListener("click",function(e){
+e.preventDefault();
+var f=document.getElementById("footer");
+if(f.style.bottom!="0px")f.style.bottom="0px";
+else f.style.bottom="-80px";});
 });
