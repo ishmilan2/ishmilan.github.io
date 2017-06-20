101b3a57e28cfce48caaca1de6e67f20a523d572
ishmilan <ish.milan.dev@gmail.com>
2017-06-20 20:27:39 +0200

    .

--- js/main.js
+++ js/main.js
@@ -9,9 +9,11 @@ document.body.appendChild(btn);
 document.addEventListener("scroll",function(){document.getElementById("scroll-top").style.bottom = "30px";});
 document.getElementById("scroll-top").addEventListener("click",function myFunction(){setTimeout(function(){document.getElementById("scroll-top").style.bottom="-80px";},200);});
 /*SHOW CONTACT FOOTER*/
+if(document.getElementById("contact")){
 document.getElementById("contact").addEventListener("click",function(e){
 e.preventDefault();
 var f=document.getElementById("footer");
 if(f.style.bottom!="0px")f.style.bottom="0px";
 else f.style.bottom="-80px";});
+}
 });
